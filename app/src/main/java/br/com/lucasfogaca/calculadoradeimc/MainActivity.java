7a8409dc3f2e4f7c2bf95d414da47dbf60568145
Lucas Fogaça <lucasfgc00@gmail.com>
2015-10-22 20:23:23 -0200

    Android
    Android AulaLPOO

---
+++ app/src/main/java/br/com/lucasfogaca/calculadoradeimc/MainActivity.java
@@ -0,0 +1,57 @@
+package br.com.lucasfogaca.calculadoradeimc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    EditText peso;
+    EditText altura;
+    TextView txt;
+    TextView resultado;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.button);
+        peso = (EditText) findViewById(R.id.peso);
+        altura = (EditText) findViewById(R.id.altura);
+        txt = (TextView) findViewById(R.id.textView2);
+        resultado = (TextView) findViewById(R.id.resultado);
+
+        txt.setText("Informe os valores a baixo:");
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Float p = Float.parseFloat(peso.getText().toString());
+                Float a = Float.valueOf(altura.getText().toString());
+
+
+                Float imc;
+
+                imc = p/(a*a);
+
+                String result;
+                result = String.valueOf(imc);
+
+                resultado.setText("IMC = " + result);
+
+            }
+        });
+
+
+
+
+    }
+}
